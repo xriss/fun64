35171c6d4a3632d5a0c044b47543afe1e09bf7cd
kriss <kriss@xixs.com>
2019-09-10 16:42:34 +0100

    windows fon files

--- art/funfont64/funfont_copy.sh
+++ art/funfont64/funfont_copy.sh
@@ -1,4 +1,5 @@
 gamecake funfont_convert.lua
 gamecake funfont_psf.lua
+./funfont_psf.sh
 gzip -kf *.psf
 cp funfont64.lua ../../../gamecake/lua/wetgenes/gamecake/fun/funfont64.lua
---
+++ art/funfont64/funfont_psf.sh
@@ -0,0 +1,15 @@
+
+
+export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib
+
+
+psf2fnt funfont_4x8b.psf  funfont_4x8b.fon
+psf2fnt funfont_8x16b.psf funfont_8x16b.fon
+psf2fnt funfont_8x16i.psf funfont_8x16i.fon
+psf2fnt funfont_8x16r.psf funfont_8x16r.fon
+
+fontforge --lang=ff -c 'Open("funfont_4x8b.fon"); Generate("funfont_4x8b.ttf")'
+fontforge --lang=ff -c 'Open("funfont_8x16b.fon"); Generate("funfont_8x16b.ttf")'
+fontforge --lang=ff -c 'Open("funfont_8x16i.fon"); Generate("funfont_8x16i.ttf")'
+fontforge --lang=ff -c 'Open("funfont_8x16r.fon"); Generate("funfont_8x16r.ttf")'
+
