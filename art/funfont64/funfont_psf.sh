b03bd6636c44236d2ce71185aae7f664f41f5ef9
kriss <kriss@xixs.com>
2019-09-10 19:19:39 +0100

    .

--- art/funfont64/funfont_psf.sh
+++ art/funfont64/funfont_psf.sh
@@ -14,8 +14,8 @@ psf2bdf funfont_8x16i.psf funfont_8x16i.bdf
 psf2bdf funfont_8x16r.psf funfont_8x16r.bdf
 
 
-fontforge -lang=py -script ./mkttf.py -V "" -A ' -a -1' -f funfont4 -w Bold    -n funfont4Bold   -N funfont4Bold   -O funfont_4x8b.bdf
+fontforge -lang=py -script ./mkttf.py -V "" -A ' -a -1' -f funfont4 -w Bold    -n funfont_4x8Bold    -N funfont_4x8Bold    -O funfont_4x8b.bdf
 
-fontforge -lang=py -script ./mkttf.py -V "" -A ' -a -1' -f funfont8 -w Bold    -n funfont8Bold   -N funfont8Bold   -O funfont_8x16b.bdf
-fontforge -lang=py -script ./mkttf.py -V "" -A ' -a -1' -f funfont8 -w Normal  -n funfont8       -N funfont8       -O funfont_8x16r.bdf
-fontforge -lang=py -script ./mkttf.py -V "" -A ' -a -1' -f funfont8 -w Italic  -n funfont8Italic -N funfont8Italic -O funfont_8x16i.bdf
+fontforge -lang=py -script ./mkttf.py -V "" -A ' -a -1' -f funfont8 -w Bold    -n funfont_8x16Bold   -N funfont_8x16Bold   -O funfont_8x16b.bdf
+fontforge -lang=py -script ./mkttf.py -V "" -A ' -a -1' -f funfont8 -w Normal  -n funfont_8x16       -N funfont_8x16       -O funfont_8x16r.bdf
+fontforge -lang=py -script ./mkttf.py -V "" -A ' -a -1' -f funfont8 -w Italic  -n funfont_8x16Italic -N funfont_8x16Italic -O funfont_8x16i.bdf
