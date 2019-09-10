579f95b8e30ce6e72bc79150b644c284f1088e6d
kriss <kriss@xixs.com>
2019-09-10 18:29:16 +0100

    bdf

--- art/funfont64/funfont_psf.sh
+++ art/funfont64/funfont_psf.sh
@@ -8,8 +8,14 @@ psf2fnt funfont_8x16b.psf funfont_8x16b.fon
 psf2fnt funfont_8x16i.psf funfont_8x16i.fon
 psf2fnt funfont_8x16r.psf funfont_8x16r.fon
 
-fontforge --lang=ff -c 'Open("funfont_4x8b.fon"); Generate("funfont_4x8b.ttf")'
-fontforge --lang=ff -c 'Open("funfont_8x16b.fon"); Generate("funfont_8x16b.ttf")'
-fontforge --lang=ff -c 'Open("funfont_8x16i.fon"); Generate("funfont_8x16i.ttf")'
-fontforge --lang=ff -c 'Open("funfont_8x16r.fon"); Generate("funfont_8x16r.ttf")'
+psf2bdf funfont_4x8b.psf  funfont_4x8b.bdf
+psf2bdf funfont_8x16b.psf funfont_8x16b.bdf
+psf2bdf funfont_8x16i.psf funfont_8x16i.bdf
+psf2bdf funfont_8x16r.psf funfont_8x16r.bdf
+
+
+#fontforge --lang=ff -c 'Open("funfont_4x8b.fon"); Generate("funfont_4x8b.ttf")'
+#fontforge --lang=ff -c 'Open("funfont_8x16b.fon"); Generate("funfont_8x16b.ttf")'
+#fontforge --lang=ff -c 'Open("funfont_8x16i.fon"); Generate("funfont_8x16i.ttf")'
+#fontforge --lang=ff -c 'Open("funfont_8x16r.fon"); Generate("funfont_8x16r.ttf")'
 
