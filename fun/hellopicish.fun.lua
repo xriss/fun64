599e70033f503acd4c9e0cb1f67c1c6a3566ded3
kriss <kriss@xixs.com>
2019-09-28 02:22:57 +0100

    canvas only example

--- fun/hellopicish.fun.lua
+++ fun/hellopicish.fun.lua
@@ -2,53 +2,16 @@
 -- This is fun64 code, you can copy paste it into https://xriss.github.io/fun64/pad/ to run it.
 --
 
-hardware,main=system.configurator({
-	mode="picish", -- select the standard 320x240 screen using the swanky32 palette.
-	update=function() update() end, -- called repeatedly to update+draw
-})
+coroutine.yield() -- wait for picish system components to exist
 
-
--- we will call this once in the update function
-setup=function()
-
---    system.components.screen.bloom=0
---    system.components.screen.filter=nil
---    system.components.screen.shadow=nil
-    
-    print("Setup complete!")
-
-end
-
-
--- updates are run at 60fps
-update=function()
-    
-    if setup then setup() setup=nil end
-
-    local ctext=system.components.text
-    local bg=9
-    local fg=system.ticks%32 -- cycle the foreground color
-
-	ctext.text_clear(0x01000000*bg) -- clear text forcing a background color
-	
-	ctext.text_print("Hello World!",(32-12)/2,7,fg,bg) -- (text,x,y,color,background)
-	
-end
-
-
---[[
-
-    local ctext=system.components.text
-    local bg=9
-    local fg=system.ticks%32 -- cycle the foreground color
+local canvas=system.components.canvas
 
 while true do
 
-	ctext.text_clear(0x01000000*bg) -- clear text forcing a background color
-	ctext.text_print("Hello World!",(32-12)/2,7,fg,bg) -- (text,x,y,color,background)
-	
-	coroutine.yield() -- end frame
+	canvas.dirty(true)
+	canvas.clear( 9 )
+	canvas.color( system.ticks%32 )
+	canvas.text( "Hello World!" , (128-(12*4))/2 , (128-8)/2 )
 
+	while not coroutine.yield().draw do end -- wait for next frame
 end
-
-]]
