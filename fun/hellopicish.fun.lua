d2ae005e6e1d34264bf6901184773fce8fcfedc4
kriss <kriss@xixs.com>
2022-12-25 05:18:49 +0000

    fix other hellos

--- fun/hellopicish.fun.lua
+++ fun/hellopicish.fun.lua
@@ -1,17 +1,19 @@
 --
 -- This is fun64 code, you can copy paste it into https://xriss.github.io/fun64/pad/ to run it.
 --
+hardware,main=system.configurator({
+	mode="picish", -- select the simple 128x128 screen using the swanky32 palette.
+	update=function() update() end, -- called repeatedly to update+draw
+})
 
-coroutine.yield() -- wait for picish system components to exist
+update=function()
 
-local canvas=system.components.canvas
-
-while true do
+	local canvas=system.components.canvas
 
 	canvas.dirty(true)
 	canvas.clear( 9 )
 	canvas.color( system.ticks%32 )
+	canvas.set_font("fun4x8")
 	canvas.text( "Hello World!" , (128-(12*4))/2 , (128-8)/2 )
 
-	while not coroutine.yield().draw do end -- wait for next frame
 end
